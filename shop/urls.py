fbcfcfd3075c113dbecb6d8bef2bc1e4e9cbed28
Lisa <lisaoloo001@gmail.com>
2021-07-02 15:00:01 +0300

    deleted categories with Ajax.

--- shop/urls.py
+++ shop/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path
 from . import views
-from shop.views import CategoryDelete, CategoryList, CategoryDetail, CategoryCreate, CategoryUpdate
+from shop.views import CategoryDelete, deleteCategory, CategoryList, CategoryDetail, CategoryCreate, CategoryUpdate, ProductList, OfferList, OrderList, VoucherList
 
 urlpatterns=[
     path("", views.home, name='home'),
-    path('categories',CategoryList.as_view(), name='category_list'),
+    path('categories',CategoryList.as_view(), name='categories'),
     path('category/detail/<pk>/', CategoryDetail.as_view(),name='category_detail'),
     path('create/category', CategoryCreate.as_view(), name='category_create'),
     path('category/update/<pk>/', CategoryUpdate.as_view(), name='category_update'),
     path('category/delete/<pk>/',CategoryDelete.as_view(), name='category_delete'),
+    path('products',ProductList.as_view(), name='product_list'),
+    path('offers',OfferList.as_view(), name='offer_list'),
+    path('vouchers',VoucherList.as_view(), name='voucher_list'),
+    path('orders',OrderList.as_view(), name='order_list'),
+    path('ajax/delete/category', views.deleteCategory, name="ajax_delete_category"),
+    
+
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Category, Seller, Product
+from .models import Category, Seller, Product, Offer, Voucher, Order    
 from django.urls import reverse
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -55,4 +56,42 @@ class ProductCreate(CreateView):
     model= Product
     fields=['name', 'cost', 'quantity', 'description', 'image', 'seller_id', 'category_id']
 
+
+class CategoryList(ListView):
+    model=Category
+    template_name= 'shop/admin/category_list.html'
+
+class ProductList(ListView):
+    model=Product
+    template_name= 'shop/admin/product_list.html'
+
+
+class OfferList(ListView):
+    model= Offer
+    template_name= 'shop/admin/offer_list.html'
+
+
+class VoucherList(ListView):
+    model=Voucher
+    template_name= 'shop/admin/voucher_list.html'
+
+
+class OrderList(ListView):
+    model=Order
+    template_name= 'shop/admin/order_list.html'
+
+
+class OrderCreate(CreateView):
+    model=Order
+    template= 'shop/admin/order_form.html'
+
+
+def deleteCategory(request):
+    category_id=request.POST.get('id', None)
+    category=Category.objects.get(id=category_id)
+    category.delete()
+    data={
+        'deleted':True
+    }
+    return JsonResponse(data)
 
