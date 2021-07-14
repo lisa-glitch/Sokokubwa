51702ed6b3a411a1582890cafb20c05a4cda6092
Lisa <lisaoloo001@gmail.com>
2021-07-14 23:55:43 +0300

    made templates for admin, created paths and created views.

--- shop/urls.py
+++ shop/urls.py
@@ -10,15 +10,58 @@ urlpatterns=[
     path('category/detail/<pk>/', CategoryDetail.as_view(),name='category_detail'),
     path('create/category', CategoryCreate.as_view(), name='category_create'),
     path('category/update/<pk>/', CategoryUpdate.as_view(), name='category_update'),
-    path('category/delete/<pk>/',CategoryDelete.as_view(), name='category_delete'),
-    path('products', ProductList.as_view(), name='product_list'),
-    path('product/create', ProductCreate.as_view(), name='product_form'),
-    path('product/update/<pk>', ProductUpdate.as_view(), name='product_update'),
-    path('offers', OfferList.as_view(), name='offer_list'),
-    path('vouchers',VoucherList.as_view(), name='voucher_list'),
-    path('orders',OrderList.as_view(), name='order_list'),
     path('ajax/delete/category', views.deleteCategory, name="ajax_delete_category"),
+
+    path('product', ProductList.as_view(), name='product_list'),
+    path('product/create', ProductCreate.as_view(), name='product_form'),
+    path('product/detail/<pk>/', ProductDetail.as_view(), name='product_detail'),
+    path('product/update/<pk>/', ProductUpdate.as_view(), name='product_update'),
+    path('product/delete/<pk>/', ProductDetail.as_view(), name='product_delete'),
+
+    path('sellers', SellerList.as_view(), name='seller_list'),
+    path('seller/create', SellerCreate.as_view(), name='seller_form'),
+    path('seller/detail/<pk>/', SellerDetail.as_view(), name='seller_detail'),
+    path('seller/update/<pk>/', SellerUpdate.as_view(), name='seller_update'),
+    path('seller/delete/<pk>/', SellerDelete.as_view(), name='seller_delete'),
     
+
+    path('offers', OfferList.as_view(), name='offer_list'),
+    path('offer/create', OfferCreate.as_view(), name='offer_form'),
+    path('offer/detail/<pk>/', OfferDetail.as_view(), name='offer_detail'),
+    path('offer/update/<pk>/', OfferUpdate.as_view(), name='offer_detail'),
+    path('offer/delete/<pk>/', OfferDelete.as_view(), name='offer_delete'),
+
+    path('vouchers',VoucherList.as_view(), name='voucher_list'),
+    path('voucher/create', VoucherCreate.as_view(), name='voucher_form'),
+    path('voucher/detail/<pk>/', VoucherDetail.as_view(), name='voucher_detail'),
+    path('voucher/update/<pk>/', VoucherUpdate.as_view(), name='voucher_update'),
+    path('voucher/delete/<pk>/', VoucherDelete.as_view(), name='voucher_delete'),
+
+
+    path('orders',OrderList.as_view(), name='order_list'),
+    path('order/create', OrderCreate.as_view(), name='order_form'),
+    path('order/detail/<pk>/', OrderDetail.as_view(), name='order_detail'),
+    path('order/update/<pk>/', OrderUpdate.as_view(), name='order_update'),
+    path('order/delete/<pk>/', OrderDelete.as_view(), name='order_delete'),
+
+    path('offers', OfferList.as_view(), name='offer_list'),
+    path('offer/create', OfferCreate.as_view(), name='offer_form'),
+    path('offer/detail/<pk>/', OfferDetail.as_view(), name='offer_detail'),
+    path('offer/update/<pk>/', OfferUpdate.as_view(), name='offer_update'),
+    path('offer/delete/<pk>/', OfferDelete.as_view(), name='offer_delete'),
+    
+    path('customers', CustomerList.as_view(), name='customer_list'),
+    path('customer/create', CustomerCreate.as_view(), name='customer_form'),
+    path('customer/create/<pk>/', CustomerUpdate.as_view(), name='customer_update'),
+    path('customer/detail/<pk>/', CustomerDetail.as_view(), name='customer_detail'),
+    path('customer/delete/<pk>/', CustomerDelete.as_view(), name='customer_delete'),
+
+    path('payments', PaymentList.as_view(), name='payment_list'),
+    path('payment/create', PaymentCreate.as_view(), name='payment_form'),
+    path('payment/detail/<pk>/', PaymentDetail.as_view(), name='payment_detail'),
+    path('payment/update/<pk>/', PaymentUpdate.as_view(), name='payment_update'),
+    path('payment/delete/<pk>/', PaymentDelete.as_view(), name='payment_delete'),
+
     
 
 
--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.edit import *
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from .models import Category, Seller, Product, Offer, Voucher, Order    
+from .models import Category, Customer, Payment, Seller, Product, Offer, Voucher, Order    
 from django.urls import reverse
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
@@ -26,6 +26,7 @@ class CategoryCreate(CreateView):
     fields='__all__'
     template_name= 'shop/admin/category_form.html'
     #function to redirect users
+
     def get_success_url(self):
         return reverse('category_list')
 
@@ -33,67 +34,69 @@ class CategoryCreate(CreateView):
 class CategoryList(ListView):
     login_required=True
     model=Category
+    fields='--all--'
     template_name= 'shop/admin/category_list.html'
+    
 
 class CategoryDetail(DetailView):
     login_required=True
     model=Category
-    login_required=True
+    fields='--all--'
+    template_name='shop/admin/category_detail.html'
 
 class CategoryUpdate(UpdateView):
     login_required=True
     model=Category
     fields=['name']
+    template_name="shop/admin/category_update.html"
     success_url="/categories"
 
-class CategoryDelete(DeleteView):
-    model=Category
-    login_required=True
-    success_url="/categories"
 
+
+    
 
 class SellerCreate(CreateView):
     login_required=True
     model= Seller
     fields= ['user_name', 'email', 'logo', 'phone number', 'status', 'password']
-    
+    template_name="shop/admin/seller_form.html"
+
     def get_success_url(self):
-        return reverse('seller_form') 
+        return reverse('seller_list') 
 
 
 class SellerList(ListView):
     login_required=True
     model= Seller
     fields= ['user_name', 'email', 'logo', 'phone number', 'status', 'password']
-    
-    def get_success_url(self):
-        return reverse('shop/admin/seller_list.html') 
+    template_name=('shop/admin/seller_list.html')
 
 
 class SellerUpdate(UpdateView):
     login_required=True
     model= Seller
     fields= ['user_name', 'email', 'logo', 'phone number', 'status', 'password']
-    
+    template_name= 'shop/admin/seller_update.html'
+
     def get_success_url(self):
-        return reverse('shop/admin/seller_update.html') 
+        return reverse('seller_list') 
 
 
 class SellerDelete(DeleteView):
     login_required=True
     model= Seller
     fields= ['user_name', 'email', 'logo', 'phone number', 'status', 'password']
-    
+    template_name= 'shop/admin/seller_delete.html'
+
     def get_success_url(self):
-        return reverse('shop/admin/seller_delete.html') 
+        return reverse('seller_list') 
 
 class SellerDetail(DetailView):
     login_required=True
     model= Seller
     fields= ['user_name', 'email', 'logo', 'phone number', 'status', 'password']
-    
-    def get_success_url(self):
-        return reverse('shop/admin/seller_detail.html') 
+    template_name='shop/admin/seller_detail.html'
+
 
 
 class ProductCreate(CreateView):
@@ -109,7 +112,7 @@ class ProductUpdate(UpdateView):
     login_required=True
     model= Product
     fields=['name', 'cost', 'quantity', 'description', 'image', 'seller_id', 'category_id']
-    template_name='shop/admin/product_form.html'
+    template_name='shop/admin/product_update.html'
 
     def get_success_url(self):
         return reverse('product_list')
@@ -119,48 +122,134 @@ class ProductDelete(DeleteView):
     login_required=True
     model= Product
     fields= ['name', 'cost', 'quantity', 'description', 'image', 'seller_id', 'category_id']
-    
+    template_name='shop/admin/product_delete.html'
     def get_success_url(self):
-        return reverse('shop/admin/product_delete.html') 
+        return reverse('product_list') 
 
 
 class ProductDetail(DetailView):
     login_required=True
     model= Product
     fields= ['name', 'cost', 'quantity', 'description', 'image', 'seller_id', 'category_id']
+    template_name='shop/admin/product_detail.html'
     
-    def get_success_url(self):
-        return reverse('shop/admin/Product_delete.html')
 
 
 class ProductList(ListView):
     login_required=True
     model=Product
-    template_name= 'product_list' 
+    fields= ['name', 'cost', 'quantity', 'description', 'image', 'seller_id', 'category_id']
+    template_name= 'shop/admin/product_list.html' 
 
+
+
+class OfferCreate(CreateView):
+    login_required= True
+    model= Offer
+    fields= ['product_id', 'offer_amount','start_date', 'end_date']
+    template_name= 'shop/admin/offer_list.html'
+
+    def get_success_url(self):
+        return reverse('offer_list')
 
 class OfferList(ListView):
-    login_required=True
+    login_required= True
     model= Offer
-    template_name= 'shop/admin/offer_list.html'
+    fields= ['product_id', 'offer_amount','start_date', 'end_date']
+    template_name= 'offer_list'
+
+class OfferDetail(DetailView):
+    login_required=True
+    model=Offer
+    fields= ['product_id', 'offer_amount','start_date', 'end_date']
+    template_name='shop/admin/offer_detail.html'
+
+class OfferDelete(DeleteView):
+    login_required=True
+    model=Offer
+    fields= ['product_id', 'offer_amount','start_date', 'end_date']
+    template_name='shop/admin/offer_delete.html'
+
+class OfferUpdate(UpdateView):
+    login_required=True
+    model=Offer
+    fields= ['product_id', 'offer_amount','start_date', 'end_date']
+    template_name='shop/admin/offer_update_html'
+
+    def get_success_url(self):
+        return reverse ('offer_list')
+
+
+
+class VoucherCreate(CreateView):
+    template_name='shop/admin/voucher_form.html'
+    model=Voucher
+    login_required=True
+    fields=['product_id', 'voucher_amount', 'voucher_tag']
+    success_url="/vouchers"
 
 
 class VoucherList(ListView):
     login_required=True
     model=Voucher
+    fields=['product_id', 'voucher_amount', 'voucher_tag']
     template_name= 'shop/admin/voucher_list.html'
-
-
-class OrderList(ListView):
+    
+class VoucherDetail(DetailView):
     login_required=True
-    model=Order
-    template_name= 'shop/admin/order_list.html'
+    model=Voucher
+    fields=['product_id', 'voucher_amount', 'voucher_tag']
+    template_name= 'shop/admin/voucher_detail.html'
+
+class VoucherUpdate(UpdateView):
+    login_required=True
+    template_name='shop/admin/voucher_update.html'
+    fields=['product_id', 'voucher_amount', 'voucher_tag']
+    model=Voucher
+    success_url='/vouchers'
+
+class VoucherDelete(DeleteView):
+    login_required=True
+    fields=['product_id', 'voucher_amount', 'voucher_tag']
+    template_name='/shop/admin/voucher_delete.html'
+    success_url='/vouchers'
+
 
 
 class OrderCreate(CreateView):
     login_required=True
     model=Order
-    template= 'shop/admin/order_form.html'
+    fields=['order_number', 'shipping_cost', 'status', 'total', 'customer_id']
+    template_name= 'shop/admin/order_form.html'
+    success_url='/orders'
+
+
+class OrderList(ListView):
+    login_required=True
+    model=Order
+    fields=['order_number', 'shipping_cost', 'status', 'total', 'customer_id']
+    template_name= 'shop/admin/order_list.html'
+
+class OrderDetail(DetailView):
+    login_required=True
+    fields=['order_number', 'shipping_cost', 'status', 'total', 'customer_id']
+    model=Order
+    template_name= 'shop/admin/order_detail.html'
+
+
+class OrderUpdate(UpdateView):
+    login_required=True
+    model=Order
+    fields=['order_number', 'shipping_cost', 'status', 'total', 'customer_id']
+    template_name= 'shop/admin/order_update.html'
+    success_url='/orders'
+
+class OrderDelete(DeleteView):
+    login_required=True
+    model=Order
+    fields=['order_number', 'shipping_cost', 'status', 'total', 'customer_id']
+    template_name= 'shop/admin/order_delete.html'
+    success_url='/orders'
 
 @login_required
 def deleteCategory(request):
@@ -172,3 +261,73 @@ def deleteCategory(request):
     }
     return JsonResponse(data)
 
+
+
+class CustomerCreate(CreateView):
+    login_required=True
+    model=Customer
+    fields=['name', 'email', 'password', 'phone_number']
+    template_name= 'shop/admin/customer_form.html'
+    success_url='/customer'
+
+class CustomerList(ListView):
+    login_required=True
+    fields=['name', 'email', 'password', 'phone_number']
+    model=Customer
+    template_name='shop/admin/customer_list.html'
+
+class CustomerDetail(DetailView):
+    template_name= 'shop,admin/customer_detail.html'
+    login_required=True
+    fields=['name', 'email', 'password', 'phone_number']
+    success_url='/customer'
+
+class CustomerUpdate(UpdateView):
+    template_name= 'shop,admin/customer_update.html'
+    login_required=True
+    fields=['name', 'email', 'password', 'phone_number']
+    success_url='/customer'
+
+
+class CustomerDelete(DeleteView):
+    login_required=True
+    model=Customer
+    fields=['name', 'email', 'password', 'phone_number']
+    template_name='shop/admin/customer_delete.html'
+    success_url='/customer'
+
+
+
+class PaymentCreate(CreateView):
+    login_required=True
+    fields=['order_id', 'mode', 'amount', 'description']
+    model=Payment
+    template_name='shop/admin/payment_form.html'
+    success_url='/payment'
+
+class PaymentList(ListView):
+    login_required=True
+    fields=['order_id', 'mode', 'amount', 'description']
+    model=Payment
+    template_name='shop/admin/payment_list.html'
+
+class PaymentDetail(DetailView):
+    login_required=True
+    fields=['order_id', 'mode', 'amount', 'description']
+    model=Payment
+    template_name='shop/admin/payment_detail.html'
+
+
+class PaymentUpdate(UpdateView):
+    login_required=True
+    model=Payment
+    fields=['order_id', 'mode', 'amount', 'description']
+    template_name='shop/admin/payment_update.html'
+    success_url='/payment'
+
+class PaymentDelete(DeleteView):
+    login_required=True
+    model=Payment
+    fields=['order_id', 'mode', 'amount', 'description']
+    template_name='shop/admin/payment_delete.html'
+    success_url='/payment'
